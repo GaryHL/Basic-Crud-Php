eb38197b0d9a289ff290ebbdc82401aa3ee334af
Gary HL <103480668+GaryHL@users.noreply.github.com>
2022-12-16 20:50:22 +0100

    create conection with the data base and basic interface

---
+++ insert.php
@@ -0,0 +1,16 @@
+<?php
+
+include('conexion.php');
+
+$con= conectar();
+
+$cod_estudiante = $_POST['cod_estudiante'];
+$dni= $_POST['dni'];
+$nombres= $_POST['nombres'];
+$apellidos= $_POST['apellidos']
+
+
+
+
+
+?>
